e332abd1888965c32f7bee88ff89761312da5302
linhapeter <linha.peter.jr@gmail.com>
2023-10-18 21:31:58 +0200

    Toggle menu function now using classes to change style

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 const toggleMenu = () => {
   const menu = document.querySelector('.dropdown-menu');
 
-  menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
+  menu.classList.toggle("dropdown-menu-off");
+  menu.classList.toggle("dropdown-menu-on");
 }
 
 const changeToggleIcon = () => {
   const icon = document.querySelector(".toggle-button").querySelector("i");
+
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-x");
   icon.classList.toggle("menu-icon");
