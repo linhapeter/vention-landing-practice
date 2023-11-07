a0befd5861f871b18ec0d8729d275364686d3483
linhapeter <linha.peter.jr@gmail.com>
2023-11-07 11:35:19 +0100

    Section reviews added plus fonts are set and ready to use

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,65 @@ const handleNavOpen = () => {
   toggleMenu();
   changeToggleIcon();
 }
+
+const createCarousel = (containerSelector) => {
+  const carouselContainer = document.querySelector(containerSelector);
+  const carousel = carouselContainer.querySelector('.carousel')
+  const slides = carouselContainer.querySelectorAll('.slide');
+  const indicatorsContainer = carouselContainer.querySelector('.slide-indicators');
+
+  let currentIndex = 0;
+
+  const updateIndicators = () => {
+    indicatorsContainer.innerHTML = '';
+    slides.forEach((slide, index) => {
+      const indicator = document.createElement('div');
+      indicator.className = 'slide-indicator';
+      if (index === currentIndex) {
+        indicator.classList.add('active');
+      }
+      indicator.addEventListener('click', () => {
+        currentIndex = index;
+        updateCarousel();
+        updateIndicators();
+      });
+      indicatorsContainer.appendChild(indicator);
+    });
+  }
+
+  const updateCarousel = () => {
+    const translateValue = -currentIndex * 100;
+    carousel.style.transform = `translateX(${translateValue}%)`;
+  }
+
+  updateIndicators();
+  updateCarousel();
+
+  let touchStartX;
+
+  carousel.addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
+  });
+
+  carousel.addEventListener('touchend', (e) => {
+    const touchEndX = e.changedTouches[0].clientX;
+    const deltaX = touchStartX - touchEndX;
+    const threshold = 50;
+
+    if (deltaX > threshold && currentIndex < slides.length - 1) {
+      currentIndex++;
+    } else if (deltaX < -threshold && currentIndex > 0) {
+      currentIndex--;
+    }
+
+    updateCarousel();
+    updateIndicators();
+  });
+}
+
+
+const carouselSections = ['.candidates-carousel', '.employers-carousel', '.headhunters-carousel'];
+
+carouselSections.forEach((section) => {
+  createCarousel(section);
+});
